5ca437ef7a39d51667ffb7661fa826074b7fc3a2
Kidd <kiddochang02@gmail.com>
2022-08-31 17:56:29 -0400

    Finished project

--- scripts.js
+++ scripts.js
@@ -7,86 +7,95 @@ function convertToWords(number) {
     } else if (number == 2) {
         number = "scissor";
     }
-
-    return number
+    
+    return number;
 }
 
 function getComputerChoice() {
     return Math.floor(Math.random() * 3);
 }
 
-function getPlayerChoice() {
-    let ps = prompt("Choose: rock paper or scissor");
-    ps = ps.toLowerCase();
-    
-    if (ps == "scissors") {
-        ps = "scissor";
-    }
-
-    if (ps == "rock") {
-        ps = 0;
-    } else if (ps == "paper") {
-        ps = 1;
-    } else if (ps == "scissor") {
-        ps = 2;
-    }
-
-    return ps;
+function playResultWin(tp, tc) {
+    win = document.getElementById("playResult");
+    win.innerHTML = ("You Win! " + tp + " beat " + tc);
 }
 
-function playRound(p,c) {
+function playResultLose(tp, tc) {
+    lose = document.getElementById("playResult");
+    lose.innerHTML = ("You Lose! " + tp + " beat " + tc);
+}
+
+function playRound(p) {
+    result = document.getElementById("playResult");
     const win = 1;
     const lose = 0;
+    c = getComputerChoice();
     tp = convertToWords(p);
     tc = convertToWords(c);
 
     if (p == c) {
         console.log("It's a Tie! " + tp + " Tied With " + tc);
-        return 2;
+        result.innerHTML = ("It's a Tie! " + tp + " tied with " + tc);
     } else if (p == 0 && c == 1) {
         console.log("You Win! " + tp + " Beat " + tc);
-        return win;
+        score(win);
+        playResultWin(tp, tc);
     } else if (p == 0 && c == 2) {
         console.log("You lose! " + tc + " Beat " + tp);
-        return lose;
+        score(lose);
+        playResultLose(tp, tc);
     } else if (p == 1 && c == 2) {
         console.log("You lose! " + tc + " Beat " + tp);
-        return lose;
+        score(lose);
+        playResultLose(tp, tc);
     } else if (p == 1 && c == 0) {
         console.log("You Win! " + tp + " Beat " + tc);
-        return win;
+        score(win);
+        playResultWin(tp, tc);
     } else if (p == 2 && c == 0) {
         console.log("You lose! " + tc + " Beat " + tp);
-        return lose;
+        score(lose);
+        playResultLose(tp, tc);
     } else if (p == 2 && c == 1) {
         console.log("You Win! " + tp + " Beat " + tc);
-        return win;
+        score(win);
+        playResultWin(tp, tc);
     } else {
         console.log("error :(")
     }
 
-}
-
-function game() {
-    let ps = 0;
-    let cs = 0;
-    let score = Number; 
-
-    for (let i = 0; i < 5; i++) {
-        score = playRound(getPlayerChoice(),getComputerChoice());
-        
-        if (score == 0 || score == 1){
-            score ? ps++ : cs++;
-        } else {}
-
-        console.log("Player Score: "+ps);
-        console.log("Computer Score: "+cs);
+    if (playerScore.textContent == "5") {
+        winner.innerHTML = "You Win!"
+    }   else if (computerScore.textContent == "5") {
+        winner.innerHTML = "You Lose!"
     }
+
 }
 
-//const player = getPlayerChoice();
-//let computer = getComputerChoice();
+function score(score) {
+    p = document.getElementById("playerScore");
+    c = document.getElementById("computerScore");
+        
+    if (score == 0 || score == 1){
+        score ? ps++ : cs++;
+    }
 
-game();
+    p.innerHTML = ps.toString();
+    c.innerHTML = cs.toString();
+    
+}
 
-//playRound(player,computer);
+let [ps,cs] = [0,0];
+let winner = document.getElementById("winner");
+let playerScore = document.getElementById("playerScore");
+let computerScore = document.getElementById("computerScore");
+
+document.getElementById("rock").addEventListener("click", function(e){
+    playRound(0);
+});
+document.getElementById("paper").addEventListener("click", function(e){
+    playRound(1);
+});
+document.getElementById("scissor").addEventListener("click", function(e){
+    playRound(2);
+});
